5934ef4c2b4cf70bca2c05f3681bc88925b0d256
MinMin_Tranova <minatran2003@gmail.com>
2025-01-30 19:50:07 +0100

    Created initial structure of project

---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById("addTaskBtn").addEventListener("click", function(){
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    const taskItem = document.createElement("li");
+    const taskContent = document.createElement("span");
+    taskContent.textContent = taskText;
+    taskContent.style.fontSize = "12px";
+    taskContent.style.alignItems = "center";
+    taskContent.style.width = "70%";
+    taskItem.appendChild(taskContent);
+
+    taskItem.addEventListener("click", function(){
+        taskItem.classList.toggle("completed");
+    });
+
+    const deleteTaskBtn = document.createElement("button");
+    deleteTaskBtn.textContent = "Delete";
+    deleteTaskBtn.style.width = "30%"
+    deleteTaskBtn.addEventListener("click", function(){
+        taskItem.remove();
+    });
+    taskItem.appendChild(deleteTaskBtn);
+
+    document.getElementById("taskList").appendChild(taskItem);
+    taskInput.value = "";
+});
